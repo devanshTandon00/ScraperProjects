b94bf23acf4e2abcddb81bcda977ff061760bdb4
devanshTandon00 <dtdansh@gmail.com>
2020-08-04 16:27:37 +0530

    Commit

--- AmazonPriceScraper/AmazonTracker.js
+++ AmazonPriceScraper/AmazonTracker.js
@@ -1,40 +1,49 @@
-require('dotenv').config()
-const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+require('dotenv').config();
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // using nightmare to create an amazon price checker
-const nightmare = require('nightmare')()
+const nightmare = require('nightmare')();
 
 // This is the argument array. Node is at position 0, fileName at position 1.
 // input url by user at position 2
-const url = process.argv[2]
+const url = process.argv[2];
 // minimum price for which user wants an alert
-const minPrice = process.argv[3]
+const minPrice = process.argv[3];
 
-priceTracker()
+const emailID = process.argv[4];
+
+priceTracker();
 
 async function priceTracker(){
-  const priceString = await nightmare.goto(url)
-            // wait for price element to load on the screen
-           .wait('#priceblock_dealprice')
-           // write js code to access price element
-           .evaluate(() => document.getElementById('priceblock_dealprice').innerText)
-           .end()
+  try{
+    const priceString = await nightmare.goto(url)
+              // wait for price element to load on the screen
+             .wait('#priceblock_ourprice')
+             // write js code to access price element
+             .evaluate(() => document.getElementById('priceblock_ourprice').innerText)
+             .end()
 
-  const formattedPriceString = parseFloat(priceString.replace('$', ''))
+    const formattedPriceString = parseFloat(priceString.replace('$', ''))
 
-  if(formattedPriceString < minPrice){
-    sendEmail(
-      'Affordable price',
-      `The item at ${url} has dropped its price below ${minPrice}`
-    )
+    if(formattedPriceString < minPrice){
+      await sendEmail(
+        'Affordable price',
+        `The item at ${url} has dropped its price below ${minPrice}`
+      )
+    }
+  }
+  catch (e) {
+    await sendEmail('Amazon Price Checker Error', e.message)
+    throw e
   }
 }
 
 // created a temp mail for testing
 function sendEmail(subject, body){
   const email = {
-    to: 'jasojil509@in4mail.net',
+    // jasojil509@in4mail.net
+    to: `${emailID}`,
     from: 'dtdansh@gmail.com',
     subject: subject,
     text: body,
@@ -42,8 +51,8 @@ function sendEmail(subject, body){
   }
 
   sgMail.send(email).then(() => {
-    console.log('Message sent')
+    console.log('Message sent');
   }).catch((error) => {
-    console.log(error.response.body)
+    console.log(error.response.body);
   })
 }
---
+++ AmazonPriceScraper/cronJob.js
@@ -0,0 +1,16 @@
+const cron = require('node-cron');
+let shell = require('shelljs');
+
+const url = process.argv[2];
+// minimum price for which user wants an alert
+const minPrice = process.argv[3];
+
+const emailID = process.argv[4];
+
+// scheduled to run every second
+cron.schedule("* * * * * *", function() {
+  console.log("running");
+  if(shell.exec(`node AmazonTracker.js ${url} ${minPrice} ${emailID}`).code !== 0){
+    console.log("Something went wrong");
+  }
+});
